2360932e282d6b936e9099ccbbc1df02267e7dfa
Gozrid <dev@gozrid.com>
2025-10-02 15:45:02 +0200

    Updated explanation

--- src/main/java/com/example/dateirechte/DateiRechteApplication.java
+++ src/main/java/com/example/dateirechte/DateiRechteApplication.java
@@ -12,19 +12,16 @@ public class DateiRechteApplication {
 
     public static void main(String[] args) {
 
-        print("Owner :", 0,0);
-        print("Gruppe:", 0,1);
-        print("Others:", 0,2);
-        print("Others:", 0,3);
+        print(0,0); // Nutzer (0) und Owner der Datei (0) sind gleich. Die Owner Permissions greifen hier
+        print(0,1); // Nutzer (0) und Owner der Datei (1) sind nicht gleich. Jedoch passt die Gruppe des Nutzers zur Gruppe der Datei. Die Gruppen Permissions greifen hier.
+        print(0,2); // Nutzer (0) und Owner der Datei (2) sind nicht gleich. Die Gruppe des Nutzers und der Datei sind unterschiedlich. Die Others Permissions greifen hier.
+        print(0,3); //  (0) und Owner der Datei (2) sind nicht gleich. Die Gruppe des Nutzers und der Datei sind unterschiedlich. Die Others Permissions greifen hier. Die Datei hat hier jedoch andere Permissions für Others.
 
     }
 
-    private static void print(String prefix, int userId, int fileId) {
+    private static void print(int userId, int fileId) {
         System.out.println(
-                prefix + " " +
-                rechteService.whatRightDoesTheUserHave(userId, fileId) + " " +
-                        fileRepository.getFile(fileId).getOwnerName() + " " +
-                        fileRepository.getFile(fileId).getGroupName());
+                rechteService.whatRightDoesTheUserHave(userId, fileId));
     }
 
 }
